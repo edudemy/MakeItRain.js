dfee76924f1bb68bcb85b896d0a035aa866e29ac
Dominik Biedebach <dominik.biedebach@gmail.com>
2014-11-11 01:41:33 +0100

    Added new features and fixes
    Limited the sprites and bind it on the window sizes. Also the sprites
    will now fade out after falling down and be removed from the DOM to
    improve performance.
    
    I also blocked multiple calls because it will turn down the performance
    when you call to many sprites at once. now you have to wait for the
    animation to finish.
    
    - plus: I added a contribution-area :) have fun!

--- js/MakeItRain.js
+++ js/MakeItRain.js
@@ -27,13 +27,13 @@ Enjoy!  And please: if you improve the code; submit a pull request!
 ///////////////////////////// CONTROL POINTS ///////////////////////////*/
 
 //------------------------- MONEY Y ORIGIN -------------------------------//
-	yOrigin = function() { return Math.random() * -1000 };
+yOrigin = function() { return Math.random() * -1000 };
 
 //------------------------- MONEY X ORIGIN -------------------------------//
-xOrigin = function() { return $.MakeItRain.random(($(window).width())) };
+xOrigin = function() { return $.MakeItRain.random(($(window).width() - 50)) };
 
 //------------------------- money Y ENDPOINT -----------------------------//
-var yLand =$(window).height() + 100;
+var yLand =$(window).height() - 20;
 
 //------------------------- money X ENDPOINT -----------------------------//
 var xLand = //$.MakeItRain.random(($(window).width()-150))	currently no x endpoint
@@ -45,22 +45,28 @@ var xLand = //$.MakeItRain.random(($(window).width()-150))	currently no x endpoi
 	var defaults = {
 		images : [
 			'images/money.png'],
-		total : 20
+		total : 20,
+		fadeTime: 400
 	};
+	var livingSprites = 0;
 
 	// SETTINGS
 	$.MakeItRain = function(settings) {
+		// Setup the living sprites to the total amount of sprites
 		$.MakeItRain.settings = $.extend({}, defaults, settings);
 			if($.MakeItRain.preloadImages()){
-				for (i = 0; i < $.MakeItRain.settings.total; i++){
-					$.MakeItRain.fall($.MakeItRain.create($.MakeItRain.settings.images[$.MakeItRain.random(($.MakeItRain.settings.images).length)]));
-				}
+					if(livingSprites <= 0) {
+						for (i = 0; i < $.MakeItRain.settings.total; i++){
+							$.MakeItRain.fall($.MakeItRain.create($.MakeItRain.settings.images[$.MakeItRain.random(($.MakeItRain.settings.images).length)]));
+						}
+					}
 			}
 		return;
 	};
 	
 	/* Public Functions */
 	$.MakeItRain.create = function(img){
+		livingSprites++;
 		spark = $('<img>').attr({'src' : img}).hide();
 		$(document.body).append(spark);
 			return spark.css({
@@ -68,15 +74,16 @@ var xLand = //$.MakeItRain.random(($(window).width()-150))	currently no x endpoi
 				'z-index': $.MakeItRain.random(20),
 				top: yOrigin,
 				left: xOrigin
-				}).show();
+				}).show().addClass('mir-sprite');
 }
 
 $.MakeItRain.fall = function(sp) {
 	$(sp).animate({
 		top: yLand,
+		opacity: 0,
 		//left: 	
 		// MakeItRain speed
-	}, (($.MakeItRain.random(100) + 100) * 85),function(){ $.MakeItRain.fall(sp) });
+	}, (($.MakeItRain.random(100) + 100) * 85),function(){ $.MakeItRain.fall(sp); $.MakeItRain.removeSprite(sp); });
 };
 
 $.MakeItRain.preloadImages = function() {
@@ -89,6 +96,12 @@ $.MakeItRain.preloadImages = function() {
 
 $.MakeItRain.random = function(max) {
 	return Math.ceil(Math.random() * max) - 1;
+}
+
+// Remove a Sprite function
+$.MakeItRain.removeSprite = function(sprite) {
+	$(sprite).remove();
+	livingSprites--;
 }
 
 })(jQuery);
